dc7253c9ca9d477dd5e7894407fa899fca18799d
CarstenHennig <carsten.hennig@gmail.com>
2021-04-14 11:17:58 +0200

    Hope its done

---
+++ js-pseudo.js
@@ -0,0 +1,62 @@
+// # Inputs and Outputs
+
+// Write down pseudo-code on pen and paper for the following patterns:
+
+// 1.
+// Input   =>	Output
+// 2,5	  =>  5
+Let a be the 1st input: 2.5 
+Multiply a with 2 
+Output will be 5
+
+let numOne = 2.5;
+console.log(numOne * 2);
+
+// 0, 100	=>  100
+Let a be the first input with the value 0
+Let b be the second input with the value 100 
+Add a and b - so the ouüput will be 100 
+
+let firstVar = 0;
+let secondVar = 100;
+console.log(firstVar + secondVar);
+
+// 52, 35	=>  52
+Let a be the first input with the value 52
+Let b be the second input with the value 35
+Display a 
+
+let figureOne = 52;
+let figureTwo = 35;
+console.log(figureOne);
+
+// 2.
+// Input	   =>   Output
+// 5, 5     =>   true
+Compare a with b and display the result, the boolean should be 'true'
+
+console.log(5 === 5);
+
+// 40, 40   =>   true
+Compare a with b and display the result, the boolean should be 'true'
+
+console.log(40 >= 40);
+
+// 50, 51   =>   false
+Check if a is equal or less than b - the result should be the boolean 'false'
+console.log(50 > 51);
+
+
+// 3.
+// Input	     =>   Output
+// 110, 100	 =>   true
+Check if a is higher than b - the result should be the boolean 'true'
+console.log(110 > 100);
+
+// 150, 100	 =>   true
+Check if a is higher than b - the result should be the boolean 'true'
+console.log(150 > 100);
+
+// 50, 100	   =>   false
+Check if a is higher or equal than b - the result should be the boolean 'false'
+console.log(50 >= 100);
